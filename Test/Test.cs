6b56a7e98677abd8cae8e8147a78d4620a10e762
Prudhvi <prudhviranjan213@gmail.com>
2025-05-29 12:23:38 +0530

    Test

---
+++ Test/Test.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+namespace ADODISHES.Test
+{
+	public class Test
+	{
+		[Theory]
+		[InlineData(1, 1, 2)]
+		[InlineData(2, -2, 0)]
+		[InlineData(3, -4, -1)]
+		[InlineData(-4, -4, -8)]
+
+		public void Test1(int a , int b, int sum)
+		{
+		{
+			Assert.Equal(a+b,sum);
+		}
+		}
+
+
+		[Fact]
+		public void Test2()
+		{
+			Assert.True(true);
+		}
+	}
+}
